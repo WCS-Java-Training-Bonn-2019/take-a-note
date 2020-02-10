f8c624656f2f135eca770465a38b6e3406e228b4
Frank Geimer <frank.geimer@telekom.de>
2020-02-10 16:37:02 +0100

    initia

---
+++ src/main/java/wildcodeschool/com/take_a_note/TakeANoteApplication.java
@@ -0,0 +1,13 @@
+package wildcodeschool.com.take_a_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeANoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeANoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wildcodeschool/com/take_a_note/TakeANoteApplicationTests.java
@@ -0,0 +1,13 @@
+package wildcodeschool.com.take_a_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeANoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
