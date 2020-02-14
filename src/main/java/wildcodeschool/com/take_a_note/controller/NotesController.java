2e2d7dd5a212018d5d06ba91a2e26a6cfd0559ce
Frank Geimer <frank.geimer@telekom.de>
2020-02-14 12:28:35 +0100

    Notizen erste Seite jetzt aus Datenbank, die editiert wurde

--- src/main/java/wildcodeschool/com/take_a_note/controller/NotesController.java
+++ src/main/java/wildcodeschool/com/take_a_note/controller/NotesController.java
@@ -1,19 +1,47 @@
 package wildcodeschool.com.take_a_note.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import wildcodeschool.com.take_a_note.repository.NotesRepository;
+
 
 
 @Controller
 public class NotesController {
+
+	
+	@Autowired
+	private NotesRepository notesRepository;
 	
 	@GetMapping("/notes")
-	
-	public String index() {
-				
-		return "seite_anzeigen_liste";
-	}
-	
 
+	public String index(Model model) {
+		model.addAttribute("notes", notesRepository.findAll());
+System.out.println(notesRepository.findAll());
+return "seite_anzeigen_liste";
+	}
+
+	@GetMapping("/anlegen")
+
+	public String indexanlegen() {
+
+		return "note_anlegen";
+	}
+
+	@GetMapping("/details")
+
+	public String indexdetails() {
+
+		return "detailansicht";
+	}
+
+	@GetMapping("/editieren")
+
+	public String indexeditieren() {
+
+		return "editieren";
+	}
 }
