a0d5a30e73aaa006b7b7d4256ea1d83b821f5717
FrankFr <f.freisberg@telekom.de>
2020-02-20 15:28:39 +0100

    Stand mit Einbindung der Seiten bis zum Anlegen

--- src/main/java/wildcodeschool/com/take_a_note/controller/NotesController.java
+++ src/main/java/wildcodeschool/com/take_a_note/controller/NotesController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import wildcodeschool.com.take_a_note.entity.Notes;
 import wildcodeschool.com.take_a_note.repository.NotesRepository;
@@ -28,12 +30,14 @@ public class NotesController {
 		return "seite_anzeigen_liste";
 	}
 
-	@GetMapping("/anlegen")
+	
+	@GetMapping("/note_anlegen")
 
 	public String indexanlegen() {
 
 		return "note_anlegen";
 	}
+	
 
 	@GetMapping("/details/{notizenId}")
 
@@ -41,16 +45,30 @@ public class NotesController {
 		
 		
 		model.addAttribute("note", notesRepository.findById(notizenId).get());      //Optional[wildcodeschool.com.take_a_note.entity.Notes@247bba1a]
-
-	
 		
 		return "details";
 	}
 
-	@GetMapping("/editieren")
+	
+	
+	
+	@GetMapping("/editieren/{notizenId}")
 
-	public String indexeditieren() {
+	public String indexeditieren(@PathVariable Long notizenId, Model model) {
+		
+		
+		model.addAttribute("note", notesRepository.findById(notizenId).get());      //Optional[wildcodeschool.com.take_a_note.entity.Notes@247bba1a]
+
 
 		return "editieren";
 	}
+	
+
+    @PostMapping("/note/upsert")
+    public String postNotes(@ModelAttribute Notes note) {
+
+    	notesRepository.save(note);
+        return "redirect:/notes";
+    }
+	
 }
